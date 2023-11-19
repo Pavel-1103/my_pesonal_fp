6fd93c5c59b4950858f78828eb84a9949ccf5904
Pavel Chernenko <chernenkopavel63@gmail.com>
2023-11-19 22:12:40 +0100

    Upgrade data in sets

--- settings/sets.py
+++ settings/sets.py
@@ -6,3 +6,18 @@ LAST_NAME = "Chernenko"
 PHONE_NUMBER = "+48696996279"
 IMPLICITLY_WAIT = 15 # Неявные ожидания
 EXPLICIT_WAIT = 15 # Явные ожидания
+
+#pytest -s -v -m "order_page" --browser_mode="gui" 
+# pytest -s -v -m "main_page" --browser_mode="gui"
+# pytest -s -v
+# -m "order_page"
+# --browser_mode="headless" 
+#  pytest -s -v -m "signup_login_page" --browser_mode="gui" 
+# --browser_name="firefox" 
+
+# pass -проходить, пропускать
+# assert - утверждать, утверждение
+# self - cамо, именно
+# present -  представляет (...например, в скобках ПАСС локатора для метода)
+# search - поиск
+# input- вход, ввод
