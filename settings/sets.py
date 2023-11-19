8c6fc00ede43c586a048c43442f199ffbb4a5464
Pavel Chernenko <chernenkopavel63@gmail.com>
2023-11-19 14:34:26 +0100

    Upgrade data in locators and sets

--- settings/sets.py
+++ settings/sets.py
@@ -1,5 +1,8 @@
 PROD_SERWER = "https://trium.com.ua/"
 TEST_EMAIL = "migog12209@marksia.com"
 PASSWORD = "Qwerty123456"
+NAME = "Pavel"
+LAST_NAME = "Chernenko"
+PHONE_NUMBER = "+48696996279"
 IMPLICITLY_WAIT = 15 # Неявные ожидания
 EXPLICIT_WAIT = 15 # Явные ожидания
